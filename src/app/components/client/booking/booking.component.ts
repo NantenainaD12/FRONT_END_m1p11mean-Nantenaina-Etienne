9d7f65efd9d8ed054c5ced3ecb363dc21623baa5
EtienneRakotoarison <etiennerakoto20@gmail.com>
2024-02-27 16:19:39 +0300

    Adding drag & drop

--- src/app/components/client/booking/booking.component.ts
+++ src/app/components/client/booking/booking.component.ts
@@ -5,11 +5,12 @@ import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, FormsM
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 import { ApiUrlService } from '../../../services/tools/api-url.service';
+import { CdkDragDrop, CdkDrag, CdkDropList, CdkDropListGroup, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CdkDropListGroup, CdkDropList, CdkDrag, RouterOutlet, CommonModule, RouterLink, RouterLinkActive, FormsModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.css'
 })
@@ -18,24 +19,23 @@ export class ClientBookingComponent {
   services: any;
   employes: any;
   bookingForm!: FormGroup;
+  todo = [];
+  done = [];
 
-  constructor(public fb: FormBuilder, public apiUrlService : ApiUrlService, public http:HttpClient, public router : Router) {}
-
-  onChange(e: any) {
-    const idServices: FormArray = this.bookingForm.get('idServices') as FormArray;
-    if (e.target.checked) {
-      idServices.push(new FormControl(parseInt(e.target.value, 10)));
+  drop(event: CdkDragDrop<never[], any, any>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      let i: number = 0;
-      idServices.controls.forEach((item: AbstractControl) => {
-        if (item.value == e.target.value) {
-          idServices.removeAt(i);
-          return;
-        }
-        i++;
-      });
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
     }
   }
+
+  constructor(public fb: FormBuilder, public apiUrlService: ApiUrlService, public http: HttpClient, public router: Router) { }
 
   ngOnInit() {
     this.getServices();
@@ -43,48 +43,56 @@ export class ClientBookingComponent {
     this.bookingForm = this.fb.group({
       dateDebut: ['', Validators.required],
       heureDebut: ['', Validators.required],
-      idEmploye: ['', Validators.required],
-      idServices: this.fb.array([])
+      idEmploye: ['', Validators.required]
     });
   }
 
   getServices() {
     this.http.get(this.apiUrlService.getUrl() + 'Manager/GetAllServices')
-        .pipe(
-          catchError(error => {
-            const jsonData = JSON.stringify(error);
-            const errorMessage = JSON.parse(jsonData).error;
-            alert(errorMessage);
-            return throwError(error);
-          })
-        )
-        .subscribe(data => {
-          this.services = JSON.parse(JSON.stringify(data));
-        });
+      .pipe(
+        catchError(error => {
+          const jsonData = JSON.stringify(error);
+          const errorMessage = JSON.parse(jsonData).error;
+          alert(errorMessage);
+          return throwError(error);
+        })
+      )
+      .subscribe(data => {
+        this.services = JSON.parse(JSON.stringify(data));
+        this.todo = this.services;
+      });
   }
 
   getEmployes() {
     this.http.get(this.apiUrlService.getUrl() + 'employe/get_employes')
-        .pipe(
-          catchError(error => {
-            const jsonData = JSON.stringify(error);
-            const errorMessage = JSON.parse(jsonData).error;
-            alert(errorMessage);
-            return throwError(error);
-          })
-        )
-        .subscribe(data => {
-          this.employes = JSON.parse(JSON.stringify(data));
-        });
+      .pipe(
+        catchError(error => {
+          const jsonData = JSON.stringify(error);
+          const errorMessage = JSON.parse(jsonData).error;
+          alert(errorMessage);
+          return throwError(error);
+        })
+      )
+      .subscribe(data => {
+        this.employes = JSON.parse(JSON.stringify(data));
+      });
+  }
+
+  hasIdService(element: any): element is { _idService: string } {
+    return typeof element._idService === 'string';
   }
 
   onSubmit() {
-    if (this.bookingForm.valid && this.bookingForm.value.idServices.length > 0) {
-      const { dateDebut, heureDebut, idEmploye, idServices } = this.bookingForm.value;
+    let idServices: any[] = [];
+    this.done.forEach(element => {
+      idServices.push(JSON.parse(JSON.stringify(element))._idService);
+    });
+    if (this.bookingForm.valid && idServices.length > 0) {
+      const { dateDebut, heureDebut, idEmploye } = this.bookingForm.value;
       var formData = new FormData();
       formData.append("dateHeureDebut", dateDebut + "T" + heureDebut + ":00");
       formData.append("idEmploye", idEmploye);
-      formData.append("idServices", idServices);
+      formData.append("idServices", idServices.join(","));
       this.http.post(this.apiUrlService.getUrl() + 'client/appointment_booking?idClient=' + this.client._idClient, formData)
         .pipe(
           catchError(error => {
